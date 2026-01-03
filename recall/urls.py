921b6e2012546cfe54224eca9c123bb94a302974
Lukasz Pudlo <l.pudlo1990@gmail.com>
2026-01-03 09:03:33 +0000

    Create basic home view

--- recall/urls.py
+++ recall/urls.py
@@ -15,8 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include("memory.urls"))
 ]
+
+if settings.DEBUG:
+    # Include django_browser_reload URLs only in DEBUG mode
+    urlpatterns += [
+        path("__reload__/", include("django_browser_reload.urls")),
+    ]
